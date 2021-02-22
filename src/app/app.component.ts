43d4aca7ef189c1243287587c315be61b860de1c
Sonia <sonia@zeptosystems.com>
2021-02-22 11:29:54 +0500

    Ng zoro blank project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,37 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'ng-zorro-app';
+// }
+
+
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <button nz-button nzType="primary">Primary Button</button>
+//     <button nz-button nzType="default">Default Button</button>
+//     <button nz-button nzType="dashed">Dashed Button</button>
+//     <button nz-button nzType="text">Text Button</button>
+//     <a nz-button nzType="link">Link Button</a>
+//   `,
+//   styles: [
+//     `
+//       [nz-button] {
+//         margin-right: 8px;
+//         margin-bottom: 12px;
+//       }
+//     `
+//   ]
+// })
+// export class AppComponent {
+//   title = 'ng-zorro-app';
+// }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HeaderComponent } from './header/header.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
